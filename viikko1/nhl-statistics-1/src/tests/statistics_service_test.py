6bd5d413337fa286355142cb4aced3819293a63f
Eeritvan <eeritvan@proton.me>
2024-10-31 20:10:37 +0200

    Completed week 1 exercise 16

---
+++ viikko1/nhl-statistics-1/src/tests/statistics_service_test.py
@@ -0,0 +1,42 @@
+import unittest
+from statistics_service import StatisticsService
+from player import Player
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Kurri",   "EDM", 37, 53),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Gretzky", "EDM", 35, 89)
+        ]
+
+class TestStatisticsService(unittest.TestCase):
+    def setUp(self):
+        # annetaan StatisticsService-luokan oliolle "stub"-luokan olio
+        self.stats = StatisticsService(
+            PlayerReaderStub()
+        )
+
+    def test_search_player(self):
+        search = self.stats.search('Semenko')
+        self.assertEqual(str(search), str(Player('Semenko', 'EDM', 4, 12)))
+
+    def test_no_player_found(self):
+        self.assertEqual(self.stats.search('...'), None)
+
+    def test_search_team(self):
+        EDMplayers = [str(i) for i in self.stats.team('EDM')]
+        result = ['Semenko EDM 4 + 12 = 16',
+                  'Kurri EDM 37 + 53 = 90',
+                  'Gretzky EDM 35 + 89 = 124']
+        
+        self.assertEqual(EDMplayers, result)
+
+    def test_top_player(self):
+        Top2Players = [str(i) for i in self.stats.top(1)]
+        result = ['Gretzky EDM 35 + 89 = 124',
+                  'Lemieux PIT 45 + 54 = 99']
+        
+        self.assertEqual(Top2Players, result)
