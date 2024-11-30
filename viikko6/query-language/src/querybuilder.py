230c4609d37eaa75b5c6f02bbe3845067f642662
eeritvan <eetu.ritvanen@helsinki.fi>
2024-11-30 19:16:35 +0200

    added week 6 ex. 4

---
+++ viikko6/query-language/src/querybuilder.py
@@ -0,0 +1,20 @@
+from matchers import And, All, Or, Not, HasAtLeast, HasFewerThan, PlaysIn
+
+class QueryBuilder:
+    def __init__(self, matcher=All()):
+        self._matcher = matcher
+
+    def _group_matchers(self, matcher):
+        return QueryBuilder(And(self._matcher, matcher))
+
+    def build(self):
+        return self._matcher
+    
+    def plays_in(self, team):
+        return self._group_matchers(PlaysIn(team))
+    
+    def has_at_least(self, value, attr):
+        return self._group_matchers(HasAtLeast(value, attr))
+
+    def has_fewer_than(self, value, attr):
+        return self._group_matchers(HasFewerThan(value, attr))
