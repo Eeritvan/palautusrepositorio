53497f61b9cff90646ad80373ba02ed2075a10d2
eeritvan <eetu.ritvanen@helsinki.fi>
2024-11-30 18:16:03 +0200

    Added week 6 ex. 3

--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -9,6 +9,19 @@ class And:
 
         return True
 
+
+class Or:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def test(self, player):
+        for matcher in self._matchers:
+            if matcher.test(player):
+                return True
+
+        return False
+
+
 class All:
     def __init__(self):
         pass
@@ -26,6 +39,7 @@ class Not:
             return True
 
         return False
+
 
 class PlaysIn:
     def __init__(self, team):
